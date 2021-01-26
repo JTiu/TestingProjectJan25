7d89fb48fe2a6f42bf4b407e532925a067c32757
james tiu <jamestiulawyer@gmail.com>
2021-01-25 20:58:45 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListAndTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> JtsList = new List<int>();
+            JtsList.Add(1);
+            Console.WriteLine(JtsList.Capacity);
+            Console.ReadLine();
+        }
+    }
+}
